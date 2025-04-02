3e89a80ce4108ca42f4c12a79299bb5a057c42db
aryan <aryansatija2003@gmail.com>
2025-04-03 00:40:52 +0530

    feat: ui improvements

--- client/my-app/src/pages/Blogs.tsx
+++ client/my-app/src/pages/Blogs.tsx
@@ -7,6 +7,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { Bot, WandSparkles } from 'lucide-react';
 import ReactMarkdown from 'react-markdown';
+import { Watermark } from 'antd';
 import '../index.css'
 interface BlogContent {
     title?: string;
@@ -64,107 +65,109 @@ const Blogs = () => {
         return <Hole/>
     }
     return (
-        <div className='relative card-sharp min-h-[100vh] overflow-x-hidden w-full min-w-[320px] p-8'>
-            {/* <div className='bg-[#1e8296] absolute top-[4rem] -z-5 left-[-35rem] h-[15.25rem] w-[15.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div> */}
-            <div className='relative z-10'>
-                <div className='bg-gradient-to-b from-[#8d8d8e] to-[#e9e6e6] inline-block text-transparent bg-clip-text text-6xl font-bold'>{blog.title}</div>
-                <div className='my-8 w-[85%] min-w-[320px] mx-auto'>{
-                    blog.content.map((para, ind)=>{
-                        return <div key = {ind}>
-                            {
-                                para.title &&
-                                <div className='text-2xl py-2 text-slate-200 font-semibold'>{para.title}</div>
-                            }
-                            {
-                                para.imageUrl && <div className='rounded-md'><img src={para.imageUrl} className='rounded-md w-[250px] md:w-[500px]'/></div>
-                            }
-                            {
-                                para.content && 
-                                <div className='text-slate-300 py-2 md:translate-x-10'>✅ {para?.content}</div>
-                            }
-                        </div>
-                    })    
-                }</div>
-                <div className='py-2 px-4 rounded-lg my-8 w-[85%] min-w-[320px] mx-auto p-4 flex flex-col items-start justify-between gap-4'>
-                    <div className='text-2xl py-2 text-gray-100 font-semibold flex flex-row items-center gap-2'>Having Doubts? Ask <div className="bg-[#319dce]/30 px-4 py-1 rounded-full text-white font-semibold cursor-pointer gap-2 flex flex-row items-center"> <Bot/> Echo</div></div>
-                    <div className='w-full items-start'>
-                        {
-                            loading && 
-                            <div>
-                                <MiniHole/>
+        <Watermark content={"CodeAssist"}>
+            <div className='relative card-sharp min-h-[100vh] overflow-x-hidden w-full min-w-[320px] p-8'>
+                {/* <div className='bg-[#1e8296] absolute top-[4rem] -z-5 left-[-35rem] h-[15.25rem] w-[15.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div> */}
+                <div className='relative z-10'>
+                    <div className='bg-gradient-to-b from-[#8d8d8e] to-[#e9e6e6] inline-block text-transparent bg-clip-text text-6xl font-bold'>{blog.title}</div>
+                    <div className='my-8 w-[85%] min-w-[320px] mx-auto'>{
+                        blog.content.map((para, ind)=>{
+                            return <div key = {ind}>
+                                {
+                                    para.title &&
+                                    <div className='text-2xl py-2 text-slate-200 font-semibold'>{para.title}</div>
+                                }
+                                {
+                                    para.imageUrl && <div className='rounded-md'><img src={para.imageUrl} className='rounded-md w-[250px] md:w-[500px]'/></div>
+                                }
+                                {
+                                    para.content && 
+                                    <div className='text-slate-300 py-2 md:translate-x-10'>✅ {para?.content}</div>
+                                }
                             </div>
-                        }
-                        {
-                            !loading && output !== '' && <ReactMarkdown>{output}</ReactMarkdown>
-                        }
-                    </div>
-                    <div className='flex flex-row items-center gap-2 w-full'>
-                        <input
-                            className='p-2 w-full text-white bg-transparent backdrop-blur-md rounded-md border focus:outline-none'
-                            type='text'
-                            placeholder='Type your doubt here✨✨'
-                            value={doubt}
-                            onChange={(e) => setDoubt(e.target.value)}
-                        ></input>
-                        <button
-                            className='bg-white py-[10px] px-[25px] rounded-md cursor-pointer text-black w-[120px]'
-                            onClick={async()=>{
-                                await askDoubt()
-                            }}
-                        >
-                            <span className='flex flex-row items-center justify-center gap-4 text-md'>Try Out <WandSparkles/></span>
-                        </button>
-                    </div>
-                 </div>
-                {
-                    blog.code &&
-                    <div className='my-8 w-[85%] min-w-[320px] mx-auto'>
-                        <div className='text-2xl py-2 text-slate-200 font-semibold'>Sample Template</div>
-                        <div className="mt-2 text-sm">
-                            <SyntaxHighlighter 
-                                language="cpp" 
-                                style={vscDarkPlus}
-                                customStyle={{
-                                    background: 'transparent',
-                                    margin: 0,
-                                    border: 'none',
-                                    overflowX: 'auto',
-                                    fontSize: '0.875rem',
-                                    whiteSpace: 'pre-wrap',
-                                    wordWrap: 'break-word',
-                                    minHeight: '10rem',
+                        })    
+                    }</div>
+                    <div className='py-2 px-4 rounded-lg my-8 w-[85%] min-w-[320px] mx-auto p-4 flex flex-col items-start justify-between gap-4'>
+                        <div className='text-2xl py-2 text-gray-100 font-semibold flex flex-row items-center gap-2'>Having Doubts? Ask <div className="bg-[#319dce]/30 px-4 py-1 rounded-full text-white font-semibold cursor-pointer gap-2 flex flex-row items-center"> <Bot/> Echo</div></div>
+                        <div className='w-full items-start'>
+                            {
+                                loading && 
+                                <div>
+                                    <MiniHole/>
+                                </div>
+                            }
+                            {
+                                !loading && output !== '' && <ReactMarkdown>{output}</ReactMarkdown>
+                            }
+                        </div>
+                        <div className='flex flex-row items-center gap-2 w-full'>
+                            <input
+                                className='p-2 w-full text-white bg-transparent backdrop-blur-md rounded-md border focus:outline-none'
+                                type='text'
+                                placeholder='Type your doubt here✨✨'
+                                value={doubt}
+                                onChange={(e) => setDoubt(e.target.value)}
+                            ></input>
+                            <button
+                                className='bg-white py-[10px] px-[25px] rounded-md cursor-pointer text-black w-[120px]'
+                                onClick={async()=>{
+                                    await askDoubt()
                                 }}
-                                codeTagProps={{ style: { fontFamily: 'Fira Code, monospace' } }}
-                                PreTag="div"
                             >
-                                {blog.code}
-                            </SyntaxHighlighter>
+                                <span className='flex flex-row items-center justify-center gap-4 text-md'>Try Out <WandSparkles/></span>
+                            </button>
                         </div>
                     </div>
-                }
-                <div className='my-8 w-[85%] min-w-[320px] mx-auto'>{
-                    blog.problems && 
-                    <div>
-                        {
-                            blog.problems.map((para, ind)=>{
-                                return <div key = {ind} className=''>
-                                    {
-                                        para.problemName &&
-                                        <div className='py-2 text-sky-500 underline font-semibold'>
-                                        <a href={para.problemLink} target='_blank'>✅ {para.problemName}</a></div>
-                                    }
-                                    <div className='text-slate-300 py-2 md:translate-x-10'>{para.hints.map((hint, ind)=>{
-                                        return <div key={ind} className='py-2'>
-                                            📌  {hint}
-                                        </div>
-                                    })}</div>
-                                </div>
-                            })    
-                        }
-                    </div>
-                }</div>
+                    {
+                        blog.code &&
+                        <div className='my-8 w-[85%] min-w-[320px] mx-auto'>
+                            <div className='text-2xl py-2 text-slate-200 font-semibold'>Sample Template</div>
+                            <div className="mt-2 text-sm">
+                                <SyntaxHighlighter 
+                                    language="cpp" 
+                                    style={vscDarkPlus}
+                                    customStyle={{
+                                        background: 'transparent',
+                                        margin: 0,
+                                        border: 'none',
+                                        overflowX: 'auto',
+                                        fontSize: '0.875rem',
+                                        whiteSpace: 'pre-wrap',
+                                        wordWrap: 'break-word',
+                                        minHeight: '10rem',
+                                    }}
+                                    codeTagProps={{ style: { fontFamily: 'Fira Code, monospace' } }}
+                                    PreTag="div"
+                                >
+                                    {blog.code}
+                                </SyntaxHighlighter>
+                            </div>
+                        </div>
+                    }
+                    <div className='my-8 w-[85%] min-w-[320px] mx-auto'>{
+                        blog.problems && 
+                        <div>
+                            {
+                                blog.problems.map((para, ind)=>{
+                                    return <div key = {ind} className=''>
+                                        {
+                                            para.problemName &&
+                                            <div className='py-2 text-sky-500 underline font-semibold'>
+                                            <a href={para.problemLink} target='_blank'>✅ {para.problemName}</a></div>
+                                        }
+                                        <div className='text-slate-300 py-2 md:translate-x-10'>{para.hints.map((hint, ind)=>{
+                                            return <div key={ind} className='py-2'>
+                                                📌  {hint}
+                                            </div>
+                                        })}</div>
+                                    </div>
+                                })    
+                            }
+                        </div>
+                    }</div>
+                </div>
             </div>
-        </div>
+        </Watermark>
     )
 }
 
--- client/my-app/src/pages/Echo.tsx
+++ client/my-app/src/pages/Echo.tsx
@@ -4,6 +4,8 @@ import Header from '../components/header';
 import Chat from '../components/chat';
 import Query from '../components/query';
 import EchoGames from './EchoGames';
+import { Modal } from "antd";
+import { MonitorPlay } from 'lucide-react';
 import "../index.css";
 const Echo = () => {
     const location = useLocation();
@@ -21,13 +23,54 @@ const Echo = () => {
     if(play === 'true'){
         return <EchoGames/>
     }
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const showModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalOpen(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalOpen(false);
+    };
     return (
     <div className='relative card-sharp h-[100vh] overflow-x-hidden w-full min-w-[320px]'>
         {/* <div className='bg-[#1e8296] absolute top-[4rem] -z-5 left-[-35rem] h-[15.25rem] w-[15.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div> */}
         <div className='py-4 w-full flex flex-col items-center justify-between h-[100vh]'>
             <Header setChat={setChat} setMode={setMode} setSession={setSession} setPlaceholder={setPlaceholder} setText={setText}/>
             <Chat isLoading={isLoading} loading={loading} chat={chat}/>
-            <Query setSession={setSession} setChat={setChat} setMode={setMode} setText={setText} setPlaceholder={setPlaceholder} setPlatform={setPlatform} mode={mode} platform={platform} session={session} text={text} placeholder={placeholder} setIsloading={setIsloading} setLoading={setLoading}/>
+            <div className={chat.length === 0  ? 'w-full lg:w-[1240px] -translate-y-80' : 'w-full' }>
+                {
+                    chat.length === 0 &&
+                    <div className='flex flex-col items-center justify-center gap-4 my-8'>
+                        <div className='text-xl font-bold'>Hello there, superstar! 🌟</div>
+                        <div className='text-xl font-bold text-center'>
+                            I’m Echo, your learning companion on this exciting journey! You’ve been doing an amazing job so far, and I’m super proud of you!
+                        </div>
+                        <div className='text-md text-center flex flex-row items-center justify-center gap-4'>
+                            <div>New here?</div>
+                            <div>
+                                <button className="cursor-pointer bg-blue-100 px-4 py-2 rounded-md text-[#0e444e]" onClick={showModal}>Watch <MonitorPlay/></button>
+                            </div>
+                        </div>
+                        <Modal title="Watch Video" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} centered>
+                            <iframe 
+                                width="100%" 
+                                height="315" 
+                                src="https://www.youtube.com/embed/g8lHmDbcde8" 
+                                title="YouTube video player" 
+                                frameBorder="0" 
+                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                                allowFullScreen
+                            ></iframe>
+                        </Modal>
+                    </div>
+                }
+                <Query setSession={setSession} setChat={setChat} setMode={setMode} setText={setText} setPlaceholder={setPlaceholder} setPlatform={setPlatform} mode={mode} platform={platform} session={session} text={text} placeholder={placeholder} setIsloading={setIsloading} setLoading={setLoading}/>
+            </div>
         </div>
     </div>
   )
